76bd9f6bbf2e069b4954b94ebdbfc0d6a2193322
fyildiz <fyildiz@localhost.localdomain>
2015-05-03 23:18:39 +0300

    forums

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+forums = ['Ruby ve Ailesi', 'Programlama Genel', 'Genel Konular', 'Otomobil Kulübü', 'Tasarım Genel', 'HTML&CSS']
+
+forums.each do |forum|
+  Forum.find_or_create_by_name(forum)
+  puts  "- #{forum} forumu oluşturuldu."
+end
