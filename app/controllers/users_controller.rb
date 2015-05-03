18f79536ea147ba0d33f88bc479f1a7de45e7cb3
fyildiz <fyildiz@localhost.localdomain>
2015-05-03 22:54:50 +0300

    oturum işlemleri

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
   before_action :select_user, only: [:show, :edit, :update, :destroy]
-  
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
+      login(@user)
       redirect_to profile_path(@user), notice: 'Aramıza hoş geldin!'
     else
       render :new
@@ -25,29 +25,29 @@ class UsersController < ApplicationController
     end
 
   end
-  
+
   def edit
     render layout: "profile"
   end
-  
+
   def update
     update_params = user_params
     if update_params.has_key?(:password)
-      update_params.delete([:password, :password_confirmation])        
+      update_params.delete([:password, :password_confirmation])
     end
-    
+
     if @user.update(update_params)
       redirect_to profile_path(@user), notice: 'Profil bilgileriniz güncellendi.'
     else
       render :edit, layout: "profile"
     end
   end
-  
+
   def destroy
     @user.destroy
-    redirect_to '/'    
+    redirect_to '/'
   end
-  
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 RailsOnForum::Application.routes.draw do
+  resource :session, only: [:new, :create, :destroy]
+  get '/oturum_ac', to: 'sessions#new', as: :login
+  get '/oturumu_kapat', to: 'sessions#destroy', as: :logout
   get '/users/new', to: redirect('/kaydol')
   get '/:id', to: 'users#show', as: :profile
   get '/:id/edit', to: 'users#edit', as: :edit_profile
