bc65e6e34d02d793c63035aa55a833869b74934c
fyildiz <fyildiz@localhost.localdomain>
2015-05-04 22:36:50 +0300

    topic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
-
+  has_many :topics, dependent: :destroy
   validates :username,    presence:   true,
-                          exclusion:  { in: [oturum_ac] },
+                          exclusion:  { in: ['oturum_ac'] },
                           uniqueness: { case_sensitive: false },
                           length:     { in: 4..12 },
                           format:     { with: /\A[a-zA-Z][a-zA-Z0-9_-]*\Z/ }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150503200706) do
+ActiveRecord::Schema.define(version: 20150504184515) do
 
   create_table "forums", force: :cascade do |t|
     t.string   "name",       null: false
@@ -20,6 +20,18 @@ ActiveRecord::Schema.define(version: 20150503200706) do
   end
 
   add_index "forums", ["name"], name: "index_forums_on_name", unique: true
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.text     "body",       null: false
+    t.integer  "user_id"
+    t.integer  "forum_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "topics", ["forum_id"], name: "index_topics_on_forum_id"
+  add_index "topics", ["user_id"], name: "index_topics_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
